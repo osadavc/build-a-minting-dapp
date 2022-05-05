0911e39329272ad2cd39a6d772c49f5f4a395839
osadavc <osadavidath@gmail.com>
2022-05-06 02:08:03 +0530

    ✨ Finish The Starter Of The Art Generator

---
+++ art-generator/README.md
@@ -0,0 +1,5 @@
+# HashLips Art Generator
+
+![Banner](/banner.png)
+
+### A Customized Version Of HashLips For The NFT Minting DAPP [Tutorial](https://osadavc.hashnode.dev/quick-start-guide-to-build-a-minting-dapp)
---
+++ art-generator/utils/update_info.js
@@ -0,0 +1,38 @@
+const basePath = process.cwd();
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+const fs = require("fs");
+
+const {
+  baseUri,
+  description,
+  namePrefix,
+  network,
+} = require(`${basePath}/src/config.js`);
+
+// read json data
+let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+let data = JSON.parse(rawdata);
+
+data.forEach((item) => {
+  if (network == NETWORK.sol) {
+    item.name = `${namePrefix} #${item.edition}`;
+    item.description = description;
+  } else {
+    item.name = `${namePrefix} #${item.edition}`;
+    item.description = description;
+    item.image = `${baseUri}/${item.edition}.png`;
+  }
+  fs.writeFileSync(
+    `${basePath}/build/json/${item.edition}.json`,
+    JSON.stringify(item, null, 2)
+  );
+});
+
+fs.writeFileSync(
+  `${basePath}/build/json/_metadata.json`,
+  JSON.stringify(data, null, 2)
+);
+
+console.log(`Updated baseUri for images to ===> ${baseUri}`);
+  console.log(`Updated description for images to ===> ${description}`);
+  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
