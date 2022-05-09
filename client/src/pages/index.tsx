110cc4a603e0f3fd91c3ef481a3ded3350da801d
osadavc <osadavidath@gmail.com>
2022-05-09 16:22:30 +0530

    ✨ Finished Frontend

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,40 @@
+import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+import {
+  useAccount,
+  useConnect,
+  useNetwork,
+  useContractWrite,
+  chain,
+} from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+import contractInterface from "../lib/BeautifulEyesNFT.json";
 
 const Home = () => {
   const [imageId, setImageId] = useState(1);
+  const [buttonMessage, setButtonMessage] = useState("Mint NFT");
+
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+  const { activeChain, switchNetworkAsync } = useNetwork();
+  const { data: account } = useAccount();
+
+  const { writeAsync } = useContractWrite(
+    {
+      addressOrName: "0x40ac75b32e02bcb48e24c7a2061b399446157f14",
+      contractInterface: contractInterface.abi,
+    },
+    "mint",
+    {
+      args: [account?.address],
+      overrides: {
+        gasLimit: 3000000,
+        value: ethers.utils.parseEther("0.005"),
+      },
+    }
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -10,6 +43,22 @@ const Home = () => {
 
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    switchNetworks();
+  }, [account]);
+
+  useEffect(() => {
+    switchNetworks();
+  }, [activeChain?.id]);
+
+  const switchNetworks = async () => {
+    if (account && activeChain?.id != chain.rinkeby.id && switchNetworkAsync) {
+      setButtonMessage("Switching Network...");
+      await switchNetworkAsync(chain.rinkeby.id);
+      setButtonMessage("Mint NFT");
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
@@ -21,9 +70,28 @@ const Home = () => {
           className="w-96 max-w-[80vh] mt-10 rounded-lg"
         />
 
-        <button className="w-full bg-[#a04edf] rounded-md mt-5 text-white py-3">
-          Connect Wallet
-        </button>
+        {!account ? (
+          <button
+            className="w-full bg-[#a04edf] rounded-md mt-5 text-white py-3"
+            onClick={() => {
+              connect();
+            }}
+          >
+            Connect Wallet
+          </button>
+        ) : (
+          <button
+            className="w-full bg-[#a04edf] rounded-md mt-5 text-white py-3 animate-pulse disabled:bg-[#a04edf]/30 disabled:animate-none disabled:cursor-none"
+            disabled={buttonMessage != "Mint NFT"}
+            onClick={async () => {
+              setButtonMessage("Minting...");
+              await writeAsync();
+              setButtonMessage("Check Your NFT On Opensea 🎉");
+            }}
+          >
+            {buttonMessage}
+          </button>
+        )}
       </div>
     </div>
   );
